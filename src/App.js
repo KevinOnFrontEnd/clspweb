c7f9e9381fcefef9104d2cdae4f5a9fdc3979d53
kevinonfrontend <kevinonfrontend@outlook.com>
2025-01-02 09:22:17 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import pkg from "clvm-lib";
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 function App() {
   const [source, setSource] = useState("");
-  const [curry, setCurry] = useState("");
+
   const [byteCode, setByteCode] = useState("");
   const [puzzleHash, setPuzzleHash] = useState("");
+  const [parameterType, setParameterType] = useState("Int");
+  const [parameterValue, setParameterValue] = useState("");
+  const [parameters, setParameters] = useState([]);
+  const [curry, setCurry] = useState("");
   const { Program } = pkg;
 
   const handleCompile = () => {
-    // Parse the Chialisp source code
-    const program = Program.fromSource(source);
+    if (source !== "") {
+      const program = Program.fromSource(source);
+      const compiledSource = program.compile();
 
-    if (curry !== "") {
-      const curryProgram = Program.fromSource(curry);
-
-      const compiledOutput = program.run(curryProgram);
-      setByteCode(compiledOutput.value.toString());
+      if (parameters.length > 0) {
+        const params = Program.fromList(parameters);
+        var output = compiledSource.value.run(params);
+        setByteCode(output.value.toString());
+      } else {
+        var output = compiledSource.value.run();
+      }
     } else {
-      // Compile the program
-      const compiledOutput = program.compile();
-      const compiledPuzzle = compiledOutput.value.toString();
-      console.log(compiledPuzzle);
-
-      setByteCode(compiledOutput.value.toString());
-
-      const puzzleHash = compiledOutput.value.hashHex();
-      setPuzzleHash(puzzleHash);
+      alert("No chialisp source code!");
     }
   };
 
@@ -36,11 +36,30 @@ function App() {
     });
   };
 
+  const handleAddParameter = () => {
+    let newParameter = {};
+
+    console.log(parameterValue);
+    //text
+    if (parameterType === "Text") {
+      newParameter = Program.fromText(parameterValue);
+    } else if (parameterType === "BigInt") {
+      //const val = BigInt(parameterValue);
+      //newParameter = Program.fromBigInt(parameterValue);
+    } else if (parameterType === "Int") {
+      const val = parseInt(parameterValue);
+      newParameter = Program.fromInt(val);
+    }
+
+    setParameters([...parameters, newParameter]);
+  };
+
   return (
     <div className="p-6 font-sans">
       {/* Warning Banner */}
       <div className="p-4 mb-6 text-center text-white bg-red-600 rounded-md">
-        ⚠️ This is a pre-alpha release and is not fully functional. Use at your own risk.
+        ⚠️ This is a pre-alpha release and is not fully functional. Use at your
+        own risk.
       </div>
 
       {/* Top Section */}
@@ -59,21 +78,38 @@ function App() {
             className="w-full p-4 text-base border border-gray-300 rounded-md resize-none"
           />
         </div>
-        {/* Curry Section */}
-        <div className="flex-2">
-          <label htmlFor="curry" className="block mb-2 font-bold text-lg">
-            Curry
-          </label>
-          <textarea
-            id="curry"
-            value={curry}
-            onChange={(e) => setCurry(e.target.value)}
-            placeholder="Enter curry code..."
-            rows="20"
-            className="w-full p-4 text-base border border-gray-300 rounded-md resize-none"
+      </div>
+
+      {/* Parameters Section */}
+      <div className="mb-6">
+        <label htmlFor="parameters" className="block mb-2 font-bold text-lg">
+          Parameters
+        </label>
+        <div className="flex items-center gap-4">
+          <select
+            value={parameterType}
+            onChange={(e) => setParameterType(e.target.value)}
+            className="p-2 text-base border border-gray-300 rounded-md"
+          >
+            <option value="Int">Int</option>
+            <option value="Text">Text</option>
+          </select>
+          <input
+            type="text"
+            value={parameterValue}
+            onChange={(e) => setParameterValue(e.target.value)}
+            placeholder="Enter value"
+            className="flex-1 p-2 text-base border border-gray-300 rounded-md"
           />
+          <button
+            onClick={handleAddParameter}
+            className="px-6 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
+          >
+            Add
+          </button>
         </div>
       </div>
+
       {/* Compile Button */}
       <div className="text-center mb-6">
         <button
@@ -82,7 +118,12 @@ function App() {
         >
           Compile
         </button>
+        <button className="flex items-center px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600 transition duration-200">
+          <PlayIcon className="w-5 h-5 mr-2" />
+          Run
+        </button>
       </div>
+
       {/* Byte Code Section */}
       <div className="mb-6">
         <label htmlFor="byteCode" className="block mb-2 font-bold text-lg">
@@ -96,6 +137,7 @@ function App() {
           className="w-full p-4 text-base border border-gray-300 rounded-md bg-gray-100 resize-none"
         />
       </div>
+
       {/* Puzzle Hash Section */}
       <div>
         <label htmlFor="puzzleHash" className="block mb-2 font-bold text-lg">
